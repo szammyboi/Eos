3c8327999068255e301aef6c1bf37976bd963f23
szammyboi <78485053+szammyboi@users.noreply.github.com>
2023-07-19 22:24:40 -0400

    Structure Changes

--- Eos.go
+++ Eos.go
@@ -3,10 +3,16 @@ package main
 import (
 	"fmt"
 
-	"github.com/szammyboi/Eos/Tools/LifxBridge"
+	"github.com/szammyboi/BitExport"
+	"github.com/szammyboi/LifxBridge"
 )
 
 func main() {
 	fmt.Println("Eos starting...")
 	LifxBridge.Discovery()
+
+	header := LifxBridge.Header{}
+	payload := LifxBridge.LightColorState{}
+	bytes := BitExport.MultipleToBytes(header, payload)
+	fmt.Println(bytes)
 }

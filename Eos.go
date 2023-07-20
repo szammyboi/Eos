32785613d1ac509fa11788f19dd3e57e72d71482
szammyboi <szammyboi@gmail.com>
2023-07-20 01:52:27 -0400

    Minor Test Changes

--- Eos.go
+++ Eos.go
@@ -3,16 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/szammyboi/BitExport"
 	"github.com/szammyboi/LifxBridge"
 )
 
 func main() {
 	fmt.Println("Eos starting...")
 	LifxBridge.Discovery()
-
-	header := LifxBridge.Header{}
-	payload := LifxBridge.LightColorState{}
-	bytes := BitExport.MultipleToBytes(header, payload)
-	fmt.Println(bytes)
 }
